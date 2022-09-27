675c047c36587f30b010b1bf5c92381482930c15
C5m7b4 <mbedingfield@dcrpos.com>
2022-09-27 10:25:37 -0500

    Add sorting by age

--- src/sort.js
+++ src/sort.js
@@ -12,4 +12,17 @@ const uniqSort = (arr, asc = true) => {
   return result.sort((a, b) => (asc ? a - b : a + b));
 };
 
-export { uniqSort };
+const uniquSortObj = (arr, key, asc = true) => {
+  const breadcrumbs = {};
+  const result = [arr[0]];
+  for (let i = 1; i < arr.length; i++) {
+    if (!breadcrumbs[arr[i][key]]) {
+      result.push(arr[i]);
+      breadcrumbs[arr[i][key]] = true;
+    }
+  }
+
+  return result.sort((a, b) => (asc ? a[key] - b[key] : b[key] - a[key]));
+};
+
+export { uniqSort, uniquSortObj };
